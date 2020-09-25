fb985db3161077b45141260d40d5e9b5f4fa95ae
teor <teor@riseup.net>
2020-09-25 22:25:31 +1000

    Update packages.nix

--- packages.nix
+++ packages.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, ... }:
 
+# make sure emacsWithPackages uses emacs-nox
+let
+  emacsWithPackages = (pkgs.emacsPackagesGen pkgs.emacs-nox).emacsWithPackages;
+in
 {
   system.autoUpgrade.enable = true;
   # 20.09 and later
@@ -19,6 +23,73 @@
   #   pinentryFlavor = "gnome3";
   # };
 
+  # create a custom emacs-nox with packages
+  nixpkgs.config = {
+    packageOverrides = pkgs: with pkgs; rec {
+      emacs-config-packages = writeText "default.el" ''
+;; initialize package
+(require 'package)
+(package-initialize)
+(package-initialize 'noactivate)
+(eval-when-compile
+  (require 'use-package))
+(eval-when-compile
+  (require 'use-package-ensure))
+(setq use-package-always-ensure t)
+
+; Show column numbers
+(setq-default column-number-mode t)
+
+; Clean trailing whitespace, show it if it happens
+(setq-default show-trailing-whitespace t)
+(use-package ws-butler
+             :ensure t)
+(ws-butler-global-mode 1)
+
+; Never use tabs to indent, show them if they happen
+(setq-default indent-tabs-mode nil)
+;; Draw tabs with the same color as trailing whitespace
+(add-hook 'font-lock-mode-hook
+    (lambda ()
+        (font-lock-add-keywords
+            nil
+            '(("\t" 0 'trailing-whitespace prepend)))))
+
+; Show key bindings
+(use-package which-key
+             :ensure t)
+      '';
+
+      emacs-nox-with-packages = emacsWithPackages (epkgs: (with epkgs; [
+        (runCommand "default.el" {} ''
+mkdir -p $out/share/emacs/site-lisp
+cp ${emacs-config-packages} $out/share/emacs/site-lisp/default.el
+        '')
+
+        use-package
+        use-package-ensure-system-package
+        auto-package-update
+        restart-emacs
+
+        which-key
+        ws-butler
+
+        rust-mode
+        lv
+        lsp-ui
+        lsp-treemacs
+        lsp-mode
+
+        flycheck
+        flycheck-rust
+        flycheck-inline
+
+        company
+        yasnippet-classic-snippets
+      ]));
+    };
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -30,18 +101,21 @@
 
     mosh
     tmux
-    pv
+    lesspipe
 
     psmisc
     htop
     hwinfo
+    file
 
     wget
     curl
+    rsync
     netcat
     mbuffer
+    pv
 
-    emacs-nox
+    emacs-nox-with-packages
 
     git
     jq
@@ -53,6 +127,7 @@
     clang
     llvm
     llvmPackages.libclang
+    lld
     binutils
     libtool
 
@@ -84,6 +159,7 @@
 
     openssl
 
+    tor
     # outdated: 2.1 from March 2020
     #zcash
   ];
